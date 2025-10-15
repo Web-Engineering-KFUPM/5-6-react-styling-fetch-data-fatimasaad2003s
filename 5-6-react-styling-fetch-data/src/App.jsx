0633d39388dd5c1cbd900a36229e95b20693a199
fatimasaad2003 <s202268600@kfupm.edu.sa>
2025-10-15 12:12:32 +0300

    TODO3

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -323,17 +323,54 @@ import SearchBar from './components/SearchBar'
 import UserModal from './components/UserModal'
 
 function App() {
-  const [users, setUsers] = useState([])
+   const [users, setUsers] = useState([]);
+   const [filteredUsers, setFilteredUsers] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+   const [searchTerm, setSearchTerm] = useState('');
+   const [showModal, setShowModal] = useState(false);
+   const [selectedUser, setSelectedUser] = useState(null);
+
 
   useEffect(() => {
-    {/*API fetch logic*/}
+   const fetchUsers = async () => {
+      setLoading(true)
+      try {
+         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+         const data = await response.json()
+         setUsers(data)
+         setFilteredUsers(data)
+      }  catch (err) {
+         setError(err.message)
+      }  finally {
+         setLoading(false)
+      }
 
+   };
+       fetchUsers();
   }, [])
 
+  useEffect(() => {
+    if (!searchTerm) {
+      setFilteredUsers(users)
+    } else {
+      const filtered = users.filter(user =>
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      setFilteredUsers(filtered)
+    }
+  }, [searchTerm, users])
+  
+  
+
   const handleUserClick = (user) => {
+   setSelectedUser(user)
+   setShowModal(true)
   }
 
   const handleCloseModal = () => {
+   setShowModal(false)
+   setSelectedUser(null)
   }
 
   return (
@@ -348,11 +385,11 @@ function App() {
       <Container className="py-3 mb-4 mt-5">
         <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
 
-        {/* {loading && <Spinner ... />} */}
-        {/* {error && <Alert ...>{error}</Alert>} */}
-        {/* <UserList users={filteredUsers} onUserClick={handleUserClick} /> */}
+        {loading && <Spinner animation="border" className="d-block mx-auto my-4" />}
+        {error && <Alert variant="danger">{error}</Alert>}
 
-        <UserModal />
+         <UserList users={filteredUsers} onUserClick={handleUserClick} />
+         <UserModal show={showModal} user={selectedUser} onHide={handleCloseModal} />
       </Container>
 
       <footer className="bg-light py-4 mt-5">
